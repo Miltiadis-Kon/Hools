75df814234e06bf2d1d89d0e17cf70ade044d9eb
Miltiadis <40666391+Miltiadis-Kon@users.noreply.github.com>
2024-03-20 19:44:42 +0200

    Append Index.html
    Add clubs
    Add matches
    Add top navigation
    Add clubs
    Add icons

--- frontend/components/login.js
+++ frontend/components/login.js
@@ -1,8 +1,6 @@
 var login = document.getElementById("login");
 login.innerHTML = `<form> 
-    <img class="mb-4" src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
     <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-
     <div class="form-floating">
         <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
         <label for="floatingInput">Email address</label>
